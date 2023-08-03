ead2750a53f0267c68ee56288ee529f0a4e83e1f
sripwoud <me@sripwoud.xyz>
2023-08-03 14:36:47 +0200

    Update static files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   baseUrl: '/',
   // Usually your repo name.
   deploymentBranch: 'gh-pages',
-  favicon: 'img/anonklub.ico',
+  favicon: 'img/favicon.ico',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
