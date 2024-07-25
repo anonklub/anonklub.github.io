6f3c25a6fd6d2202948656bd788ead31dcf16edb
sripwoud <me@sripwoud.xyz>
2024-07-25 08:01:02 +0200

    chore: remove unused files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import Link from '@docusaurus/Link'
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
-import Layout from '@theme/Layout'
-import styles from './index.module.css'
+import React from 'react';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import styles from './index.module.css';
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext()
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout>
       <div className={styles.titleContainer}>
-        <img src='/img/anonklub.svg' width={200} height={200} alt='logo' />
-        <h1 className='hero__title'>{siteConfig.title}</h1>
-        <p className='hero__subtitle'>{siteConfig.tagline}</p>
+        <img src="/img/anonklub.svg" width={200} height={200} alt="logo" />
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div>
           <Link
-            className='button button--secondary button--lg'
-            to='/docs/intro'
+            className="button button--secondary button--lg"
+            to="/docs/intro"
           >
             Example Tutorial
           </Link>
         </div>
       </div>
     </Layout>
-  )
+  );
 }
